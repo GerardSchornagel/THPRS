00593814f36aa5f9c676b7faf1824bd9768770dc
Gerard Evert Schornagel <gschornagel@hotmail.com>
2023-08-08 15:19:24 +0200

    Initial creation of configuration Internet availability check

---
+++ ConfigurationManager.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using Newtonsoft.Json;
+using THPRS;
+
+namespace THPRS
+{
+    class ConfigurationManager
+    {
+        private static string configFilePath = "config.json";
+
+        public static void WriteConfiguration(Configuration config)
+        {
+            string jsonString = JsonConvert.SerializeObject(config, Formatting.Indented);
+            File.WriteAllText(configFilePath, jsonString);
+        }
+
+        public static Configuration ReadConfiguration()
+        {
+            if (File.Exists(configFilePath))
+            {
+                string jsonString = File.ReadAllText(configFilePath);
+                return JsonConvert.DeserializeObject<Configuration>(jsonString);
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+
+    class Configuration
+    {
+        public bool InternetAvailability { get; set; }
+    }
+}
